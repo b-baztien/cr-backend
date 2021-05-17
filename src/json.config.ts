7fa35671285623e43fcb36a3570f6ee1a78dc3a6
POOMIN YENNATTEE <poomin.y@cdg.co.th>
2021-05-18 03:38:12 +0700

    add promise

--- src/json.config.ts
+++ src/json.config.ts
@@ -1,22 +1,22 @@
 import { Course } from './interfaces/course.interface';
 
 export class JsonConfig {
-  private _jsonFilePath: string = './assets/json/courses.json';
+  private _jsonFilePath: string = './assets/json/coursess.json';
   private fs = require('fs');
 
   public get getJsonFilePath(): string {
     return this._jsonFilePath;
   }
 
-  readJsonFile = () => {
-    return this.fs.readFile(this._jsonFilePath, 'utf8', (err, jsonString) => {
-      if (err) {
-        console.log('อ่านไฟล์ล้มเหลว:', err);
-        return;
-      }
-
-      console.log('ข้อมูล:', jsonString);
-      return JSON.parse(jsonString);
+  readJsonFile = () =>
+    new Promise<Course[]>((resolve, reject) => {
+      this.fs.readFile(this._jsonFilePath, 'utf8', (err, jsonString) => {
+        if (err) {
+          console.log('อ่านไฟล์ล้มเหลว:', err);
+          reject(err);
+          return;
+        }
+        resolve(JSON.parse(jsonString));
+      });
     });
-  };
 }
